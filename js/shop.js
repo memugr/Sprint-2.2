f6ddba5f3869234aed75772cb9bc40f9226bc98a
mei <132495305+memugr@users.noreply.github.com>
2024-08-18 18:57:56 +0200

    Link base + preparation
    - Linked base repo from https://github.com/IT-Academy-BCN/starter-code-frontend-shop
    - HTML: updated description, metadata, links, and emptied cart_list as default
    - JS: created a new file for the products

---
+++ js/shop.js
@@ -0,0 +1,43 @@
+// => Reminder, it's extremely important that you debug your code. 
+// ** It will save you a lot of time and frustration!
+// ** You'll understand the code better than with console.log(), and you'll also find errors faster. 
+// ** Don't hesitate to seek help from your peers or your mentor if you still struggle with debugging.
+
+// Improved version of cartList. Cart is an array of products (objects), but each one has a quantity field to define its quantity, so these products are not repeated.
+var cart = [];
+var total = 0;
+
+// Exercise 1
+function buy(id) {
+}
+
+// Exercise 2
+function cleanCart() {
+}
+
+// Exercise 3
+function calculateTotal() {
+    // Calculate total price of the cart using the "cartList" array
+}
+
+// Exercise 4
+function applyPromotionsCart() {
+    // Apply promotions to each item in the array "cart"
+}
+
+// Exercise 5
+function printCart() {
+    // Fill the shopping cart modal manipulating the shopping cart dom
+}
+
+
+// ** Nivell II **
+
+// Exercise 7
+function removeFromCart(id) {
+
+}
+
+function open_modal() {
+    printCart();
+}
